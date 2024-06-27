78497de0c8d0b85dcc18f07d58c7d81c892463f6
Bioparco <monnimarco90@gmail.com>
2024-06-27 21:58:36 +0200

    refactoring

---
+++ src/FantaMauiApp/Components/Pages/Home.razor.cs
@@ -0,0 +1,46 @@
+﻿using FantaMauiApp.Data;
+using Model;
+using System.Diagnostics;
+
+
+namespace FantaMauiApp.Components.Pages
+{
+    public partial class Home
+    {
+        public string? TeamName { get; set; } = "";
+        private Team? Team { get; set; }
+        private List<Team> teams = [];
+
+        private async Task AddTeam()
+        {
+            Team team = new() { Name = "Test" };
+            Repository<Team> repo = new(new());
+            await repo.InsertAsync(team);
+        }
+
+        private async Task GetTeams() => teams = await TeamRepository.GetAllAsync();
+
+        private async Task DeleteItem(Team item)
+        {
+            Debug.WriteLine(item.Name);
+
+            try
+            {
+                await TeamRepository.DeleteAsync(item);
+                await GetTeams();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error deleting team: {ex.Message}");
+            }
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            await GetTeams();
+
+
+        }
+    }
+}
