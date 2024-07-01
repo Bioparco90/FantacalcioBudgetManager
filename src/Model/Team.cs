d8780c441a022893e572f027bb148cf5df3992bc
Bioparco <monnimarco90@gmail.com>
2024-07-01 23:03:35 +0200

    Model changed

--- src/Model/Team.cs
+++ src/Model/Team.cs
@@ -2,6 +2,8 @@
 {
     public class Team : DataObject
     {
+        public string? League { get; set; }
+
         public virtual ICollection<Goalkeeper>? Goalkeepers { get; }
         public virtual ICollection<Defender>? Defenders { get; }
         public virtual ICollection<Midfielder>? Midfielders { get; }
