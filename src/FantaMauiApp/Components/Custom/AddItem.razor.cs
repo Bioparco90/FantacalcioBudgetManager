f062fbecedc0cdfff7c4f17bf297c864b668ecbf
Bioparco <monnimarco90@gmail.com>
2024-07-02 23:18:54 +0200

    Pages in progress

--- src/FantaMauiApp/Components/Custom/AddItem.razor.cs
+++ src/FantaMauiApp/Components/Custom/AddItem.razor.cs
@@ -1,14 +1,16 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Model;
+using Radzen;
 
 namespace FantaMauiApp.Components.Custom
 {
     public partial class AddItem<TEntity> where TEntity : DataObject, new()
     {
         [Parameter] public EventCallback<TEntity> OnAdd { get; set; }
-        [Parameter] public string? ItemTypeName { get; set; } = string.Empty;
 
         private string ItemName { get; set; } = string.Empty;
+        private string League {  get; set; } = string.Empty;
+        private string? Price { get; set; } = string.Empty;
 
         private async Task AddItemBtnClick()
         {
@@ -21,8 +23,35 @@ namespace FantaMauiApp.Components.Custom
             {
                 Name = ItemName,
             };
+
+            if (item is Team t)
+            {
+                t.League = League;
+            }
+
+            if (item is Player p)
+            {
+                if (int.TryParse(Price, out int price))
+                {
+                    p.Price = price;
+                }
+                else
+                {
+                    _ = await DialogService.Alert("Please insert a valid number", "Error", new() { CloseDialogOnOverlayClick = true, OkButtonText = "Close" });
+                    Reset();
+                    throw new FormatException();
+                }
+            }
+
             await OnAdd.InvokeAsync(item);
+            Reset();
+        }
+
+        private void Reset()
+        {
             ItemName = string.Empty;
+            League = string.Empty;
+            Price = string.Empty;
         }
     }
 }
