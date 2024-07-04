3a7d6c9c22da0c859feb892cb143bc23328add4b
Bioparco <monnimarco90@gmail.com>
2024-07-04 16:47:50 +0200

    Interface created

---
+++ src/FantaMauiApp/Data/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using Model;
+
+namespace FantaMauiApp.Data.Interfaces
+{
+    interface IRepository<T>
+    {
+        Task<int> DeleteAsync(T item);
+        Task<List<T>> GetAllAsync();
+        Task<T?> GetAsync(T item);
+        Task<int> InsertAsync(T item);
+    }
+}
